b55e158f7f6200b221545f6f8f4e10ff64371834
gayan830 <malshan95104@gmail.com>
2021-07-21 16:35:26 +0530

    ADD: Login / Logout

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,174 +1,173 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import {useParams} from 'react-router-dom'
-import db from '../firebase'
-import { setMovies } from '../features/movie/movieSlice';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import db from "../firebase";
 
 function Detail() {
+  const { id } = useParams();
+  const [ movie, setMovies ] = useState();
 
-    const { id } = useParams();
-    const { movie, setMovie} = useState();
-
-    useEffect(() => {
-        // Grab movie info from db
-        db.collection("movies")
-        .doc(id)
-        .get()
-        .then(doc => {
-            if(doc.exists){
-                //save the movie data
-                setMovies(doc.data())
-            } else {
-                //redirect home page
-                
-            }
-        })
-    }, [])
-
-    return (
-        <Container >
-            <Background>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" />
-            </Background>
-            <ImageTitle>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
-            </ImageTitle>
-            <Controls>
-                <PlayButton>
-                    <img src="/images/play-icon-black.png" />
-                    <span>PLAY</span>
-                </PlayButton>
-                <TrailerButton>
-                    <img src="/images/play-icon-white.png" />
-                    <span>TRAILER</span>
-                </TrailerButton>
-                <AddButton>
-                    <span>+</span>
-                </AddButton>
-                <GroupWatchButton>
-                    <img src="/images/group-icon.png" />
-                </GroupWatchButton>
-            </Controls>
-            <SubTitle>
-                2018.7m.Family, Kids, Animation
-            </SubTitle>
-            <Description>
-                A Chinese mom who's sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life as a lively, giggly dumpling boy.
-            </Description>
-        </Container>
-    )
+  useEffect(() => {
+    // Grab movie info from db
+    db.collection("movies")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          //save the movie data
+          setMovies(doc.data());
+        } else {
+          //redirect home page
+        }
+      });
+  }, [id]);
+  console.log(movie);
+  return (
+    <Container>
+      {movie && (
+        <>
+          <Background>
+            <img src={movie.backgroundImg} />
+          </Background>
+          <ImageTitle>
+            <img src={movie.titleImg} />
+          </ImageTitle>
+          <Controls>
+            <PlayButton>
+              <img src="/images/play-icon-black.png" />
+              <span>PLAY</span>
+            </PlayButton>
+            <TrailerButton>
+              <img src="/images/play-icon-white.png" />
+              <span>TRAILER</span>
+            </TrailerButton>
+            <AddButton>
+              <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+              <img src="/images/group-icon.png" />
+            </GroupWatchButton>
+          </Controls>
+          <SubTitle>{movie.SubTitle}</SubTitle>
+          <Description>
+            A Chinese mom who's sad when her grown son leaves home gets another
+            chance at motherhood when one of her dumplings springs to life as a
+            lively, giggly dumpling boy.
+          </Description>
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default Detail
+export default Detail;
 
 const Container = styled.div`
-    min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px);
-    position: relative;
-    overflow: hidden;
-`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow: hidden;
+`;
 
 const Background = styled.div`
-    position: fixed;
-    top:0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: -1;
-    opacity: 0.8;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
 
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 const ImageTitle = styled.div`
-    height: 30vh;
-    min-height: 170px;
-    margin-top: 20px;
-    width: 35vw;
-    min-width: 600px;
+  height: 30vh;
+  min-height: 170px;
+  margin-top: 20px;
+  width: 35vw;
+  min-width: 600px;
 
-    img {
-        height: 100%;
-        width: 100%;
-        object-fit: contain;
-    }
-`
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+`;
 
 const Controls = styled.div`
-    display: flex;
-    align-items: center;
-    
-`
+  display: flex;
+  align-items: center;
+`;
 
 const PlayButton = styled.div`
-    border-radius: 4px;
-    font-size: 15px;
-    padding: 0 24px;
-    margin-right: 22px;
-    display: flex;
-    align-items: center;
-    height: 56px;
-    background: rgb(249, 249, 249);
-    border: none;
-    letter-spacing: 1.8px;
-    cursor: pointer;
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
 
-    span {
-        color: #090b13;
-    }
+  span {
+    color: #090b13;
+  }
 
-    &:hover {
-        background: rgb(198, 198, 198);
-    }
-`
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
 
 const TrailerButton = styled(PlayButton)`
-    background: rgba(0, 0, 0, 0.3);
-    border: 1px solid rgb(249, 249, 249);
-    color: rgb(249, 249, 249);
-    text-transform: uppercase;
-    span {
-        color: white;
-    }
-`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+  span {
+    color: white;
+  }
+`;
 
-const AddButton =styled.div`
-    margin-right: 16px;
-    width: 44px;
-    height: 44px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    border: 2px solid white;
-    background-color: rgba(0, 0, 0, 0.6);
-    cursor: pointer;
+const AddButton = styled.div`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
 
-    span {
-        font-size: 30px;
-        color: white;
-    }
-`
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
 
 const GroupWatchButton = styled(AddButton)`
-    background: rgb(0, 0, 0);
-
-`
+  background: rgb(0, 0, 0);
+`;
 
 const SubTitle = styled.div`
-    color: rgb(249, 249, 249);
-    font-size: 15px;
-    min-height: 20px;
-    margin-top: 26px;
-`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
 
 const Description = styled.div`
-    line-height: 1.4;
-    font-size: 20px;
-    margin-top: 16px;
-    max-width: 700px;
-`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  max-width: 700px;
+`;
